19be582172ae8441deddb826735061253eae85d0
Milan-imca <milan.imca22@gmail.com>
2023-12-09 11:04:34 +0530

    Practice folder added

--- Practice/src/Arrays/Basic.java
+++ Practice/src/Arrays/Basic.java
@@ -2,6 +2,6 @@ package Arrays;
 
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("Welcome to arrays");
+        System.out.println("Programs will be coming soon!");
     }
 }
