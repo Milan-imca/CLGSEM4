61aad7bea4672a46a5b325be1dc880d9402c2b6d
Milan-imca <milan.imca22@gmail.com>
2024-01-13 10:55:21 +0530

    edit 17 no

--- PL_SQL/Assignment-2/17.sql
+++ PL_SQL/Assignment-2/17.sql
@@ -1,20 +1,15 @@
 -- 17. Write a PL/SQL block to calculate the sum of digits of a number entered through keyboard.
 
-
 DECLARE
   num NUMBER := &num;
   digit NUMBER;
   sum_of_digits NUMBER := 0;
 
 BEGIN
-  -- DBMS_OUTPUT.PUT_LINE('Enter a number: ');
- -- DBMS_OUTPUT.GET_LINE(num);  -- Accept input from keyboard
---  num := &num;
-
   WHILE num > 0 LOOP
     digit := MOD(num, 10);  -- Extract the last digit
     sum_of_digits := sum_of_digits + digit;
-    num := num / 10;  -- Remove the last digit
+    num := trunc(num / 10);  -- Remove the last digit
   END LOOP;
 
   DBMS_OUTPUT.PUT_LINE('Sum of digits = ' || sum_of_digits);
