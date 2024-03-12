dfd46ed3020605d7794f7a83e79cad5c9c4a0650
Milan-imca <milan.imca22@gmail.com>
2024-03-12 09:52:24 +0530

    edited

--- DS/Searching/BinarySearch.java
+++ DS/Searching/BinarySearch.java
@@ -1,12 +1,12 @@
 public class BinarySearch {
   public static void main(String[] args) {
-    int[] arr = { 4, 7, 11, 17, 19, 20, };
-    int target = 20;
+    int[] arr = { 10, 11, 90, 3, 34 };
+    int target = 31;
     int result = search(arr, target);
     if (result != -1) {
-      System.out.println(target + " was found in " + result + " position");
+      System.out.println(target + " was found ");
     } else {
-      System.out.println(target + " was element was not found!");
+      System.out.println(target + "  was not found!");
     }
   }
 
@@ -14,6 +14,7 @@ public class BinarySearch {
     int start = 0;
     int end = arr.length - 1;
 
+    sort(arr);
     while (start <= end) {
       int mid = start + (end - start) / 2;
       if (target < arr[mid]) {
@@ -25,6 +26,18 @@ public class BinarySearch {
       }
     }
     return -1;
+  }
+
+  public static void sort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
   }
 
 }
